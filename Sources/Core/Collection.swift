a2eab6c2b71ef40a24a63e6cbb62e39d72801985
leohidalgo <leohidalgo@protonmail.com>
2024-12-06 12:33:52 -0300

    feat(core): add Collection.allDifferent

--- Sources/Core/Collection.swift
+++ Sources/Core/Collection.swift
@@ -2,5 +2,7 @@ import Foundation
 
 extension Collection where Element: Hashable {
 
+    public var allDifferent: Bool { Set(self).count == count }
+
     public var allSame: Bool { Set(self).count == 1 }
 }
