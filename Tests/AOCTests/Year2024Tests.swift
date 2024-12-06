384cd74d5c03386e826fb8852ede6edea09b608e
leohidalgo <leohidalgo@protonmail.com>
2024-12-06 16:53:29 -0300

    feat(year-2024): add Day 2

--- Tests/AOCTests/Year2024Tests.swift
+++ Tests/AOCTests/Year2024Tests.swift
@@ -12,4 +12,12 @@ struct Year2024Tests {
         #expect(sut.part1() == 2031679, "Part 1")
         #expect(sut.part2() == 19678534, "Part 2")
     }
+
+    @Test
+    func day2() {
+        let sut = Day2()
+
+        #expect(sut.part1() == 624, "Part 1")
+        #expect(sut.part2() == 658, "Part 2")
+    }
 }
