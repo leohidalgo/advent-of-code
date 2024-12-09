836217b216c15b7405afed72ccffd91e1dfdc1dc
leohidalgo <leohidalgo@protonmail.com>
2024-12-09 14:46:45 -0300

    feat(year-2024): add Day 2 (#11)

---
+++ Sources/Year2024/Day 2/Day2.swift
@@ -0,0 +1,36 @@
+import Core
+import Foundation
+
+struct Day2: Day {
+
+    func part1() -> Int {
+        input().lines
+            .map(\.integers)
+            .count(where: isSafe)
+    }
+
+    func part2() -> Int {
+        input().lines
+            .map(\.integers)
+            .count { values in
+                isSafe(values) || isDampenerSafe(values)
+            }
+    }
+}
+
+private func isSafe(_ values: [Int]) -> Bool {
+    let levels = zip(values.dropFirst(), values).map(-)
+
+    return
+        levels.map(\.signum).allSame &&
+        levels.map(abs).allSatisfy { $0 <= 3 }
+}
+
+private func isDampenerSafe(_ values: [Int]) -> Bool {
+    (0..<values.count).contains { index in
+        var new = values
+        new.remove(at: index)
+
+        return isSafe(new)
+    }
+}
