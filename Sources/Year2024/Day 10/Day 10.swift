18df2dbc6d08465718d3bbd266a1150ad98d076d
leohidalgo <leohidalgo@protonmail.com>
2024-12-19 19:08:33 -0300

    feat(year-2024): add Day 10

---
+++ Sources/Year2024/Day 10/Day 10.swift
@@ -0,0 +1,62 @@
+import Core
+import Foundation
+
+struct Day10: Day {
+
+    func part1() -> Int {
+        let map = input().lines.map(\.characters).map { $0.compactMap(Int.init) }
+
+        var answer = 0
+        for (i, line) in map.enumerated() {
+            for (j, item) in line.enumerated() where item == 0 {
+                answer += Set(Self.dfs(i, j, 0, map)).count
+            }
+        }
+
+        return answer
+    }
+
+    func part2() -> Int {
+        let map = input().lines.map(\.characters).map { $0.compactMap(Int.init) }
+
+        var answer = 0
+        for (i, line) in map.enumerated() {
+            for (j, item) in line.enumerated() where item == 0 {
+                answer += Self.dfs(i, j, 0, map).count
+            }
+        }
+
+        return answer
+    }
+
+    private static func dfs(_ row: Int, _ col: Int, _ target: Int, _ map: [[Int]]) -> [Point] {
+        guard
+            row >= 0 && row < map.count,
+            col >= 0 && col < map[0].count,
+            target == map[row][col]
+        else { return [] }
+
+        guard
+            target != 9
+        else { return [Point(row, col)] }
+
+        var answer: [Point] = []
+
+        answer.append(contentsOf: dfs(row - 1, col, target + 1, map))
+        answer.append(contentsOf: dfs(row + 1, col, target + 1, map))
+        answer.append(contentsOf: dfs(row, col - 1, target + 1, map))
+        answer.append(contentsOf: dfs(row, col + 1, target + 1, map))
+
+        return answer
+    }
+}
+
+private struct Point: Hashable {
+    let row: Int
+    let col: Int
+
+    init(_ row: Int, _ col: Int) {
+        self.row = row
+        self.col = col
+    }
+}
